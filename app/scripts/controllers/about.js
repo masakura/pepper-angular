98e0fc29444530db2556a7ab9e6a34fc979d980a
Tomo Masakura <tomo.masakura@gmail.com>
2015-10-15 19:35:38 +0900

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pepperAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pepperAngularApp
+ */
+angular.module('pepperAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pepperAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pepperAngularApp
+ */
+angular.module('pepperAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
