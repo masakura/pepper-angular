f6739556cb94cfccb7011993f33442063b2070ba
Tomo Masakura <tomo.masakura@gmail.com>
2015-10-21 20:12:26 +0900

    料理情報を動的に取得するように修正

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,18 @@
  */
 angular.module('pepperAngularApp')
   .controller('MainCtrl', function ($window, qiSession) {
+    this.cooks = [
+      {
+        id: 'cook0001',
+        name: 'だんぼ',
+        image: 'images/image-01.jpg'
+      }, {
+        id: 'cook0002',
+        name: '串揚げ',
+        image: 'images/image-02.jpg'
+      },
+    ];
+
     this.click = function () {
       qiSession.service("ALMemory").done(function (ALMemory) {
         console.log("ALMemory取得成功");
